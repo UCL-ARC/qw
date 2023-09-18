ac5f2789dea978a4953532b3e9efb6e5e26683ed
Stef Piatek <s.piatek@ucl.ac.uk>
2023-09-18 12:44:39 +0100

    Add in some prompts and confirmations

--- src/qw/cli.py
+++ src/qw/cli.py
@@ -11,7 +11,12 @@ app = typer.Typer()
 def parrot(
         example: Annotated[str, typer.Argument(help="String to print to user")]
 ) -> None:
-    logger.info("🦜 {example}", example=example)
+    parrot_name = typer.prompt("What's your parrot's name?", default="Polly")
+    shout = typer.confirm("Do you want your parrot to shout?")
+    output_message = example
+    if shout:
+        output_message = f"{output_message.upper()}!!"
+    logger.info("{parrot_name}🦜: {output_message}", parrot_name=parrot_name, output_message=output_message)
 
 
 @app.command()
