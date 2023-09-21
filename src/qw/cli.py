4a57148c4e5f5a930fe80686700657c39f8602d6
Tim Band <t.band@nnnn.com>
2023-09-21 10:26:34 +0100

    reformatted with black

--- src/qw/cli.py
+++ src/qw/cli.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-import typer as typer
+import typer
 from loguru import logger
 
 # Main entry point into the application
@@ -9,19 +9,23 @@ app = typer.Typer()
 
 @app.command()
 def parrot(
-        example: Annotated[str, typer.Argument(help="String to print to user")]
+    example: Annotated[str, typer.Argument(help="String to print to user")],
 ) -> None:
     parrot_name = typer.prompt("What's your parrot's name?", default="Polly")
     shout = typer.confirm("Do you want your parrot to shout?")
     output_message = example
     if shout:
         output_message = f"{output_message.upper()}!!"
-    logger.info("{parrot_name}🦜: {output_message}", parrot_name=parrot_name, output_message=output_message)
+    logger.info(
+        "{parrot_name}🦜: {output_message}",
+        parrot_name=parrot_name,
+        output_message=output_message,
+    )
 
 
 @app.command()
 def echo(
-        example: Annotated[str, typer.Argument(help="String to print to user")]
+    example: Annotated[str, typer.Argument(help="String to print to user")],
 ) -> None:
     logger.info("Echo: {example}", example=example)
 
