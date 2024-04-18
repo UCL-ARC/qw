012fc27c53bf1a1f0a2a60808e56507b1295fcea
Tim Band <t.band@nnnn.com>
2024-04-18 11:46:25 +0100

    Fixed pre-commit checks

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,21 +1,42 @@
-"""Test check behaviour"""
+"""Test check behaviour."""
 import pytest
 
 from qw.cli import run_checks_with_impacts
 from qw.design_stages.main import get_local_stages
 
+
 @pytest.mark.parametrize(
-    ("empty_local_store", "test_design_stages", "expected_error_count", "expected_warning_count"), [
-        ({ "checks": {
-            "User need links have qw-user-need label": "warning",
-            "User Need links must exist": "error",
-            "Closing Issues are Requirements": "off",
-        }}, "incorrect_links", 1, 2),
-        ({ "checks": {
-            "User need links have qw-user-need label": "off",
-            "User Need links must exist": "off",
-            "Closing Issues are Requirements": "warning",
-        }}, "incorrect_links", 0, 2),
+    (
+        "empty_local_store",
+        "test_design_stages",
+        "expected_error_count",
+        "expected_warning_count",
+    ),
+    [
+        (
+            {
+                "checks": {
+                    "User need links have qw-user-need label": "warning",
+                    "User Need links must exist": "error",
+                    "Closing Issues are Requirements": "off",
+                },
+            },
+            "incorrect_links",
+            1,
+            2,
+        ),
+        (
+            {
+                "checks": {
+                    "User need links have qw-user-need label": "off",
+                    "User Need links must exist": "off",
+                    "Closing Issues are Requirements": "warning",
+                },
+            },
+            "incorrect_links",
+            0,
+            2,
+        ),
     ],
     indirect=["empty_local_store", "test_design_stages"],
 )
@@ -25,9 +46,14 @@ def test_check_severity(
     expected_error_count,
     expected_warning_count,
 ):
+    """
+    Test check severity configuration.
+
+    It should effect which errors and warnings are reported.
+    """
     store = qw_store_builder(test_design_stages)
     stages = get_local_stages(store)
     results = run_checks_with_impacts(store, stages)
-    assert results.object_count == 6
+    assert results.object_count == len(store.read_local_data())
     assert len(results.errors) == expected_error_count
     assert len(results.warnings) == expected_warning_count
